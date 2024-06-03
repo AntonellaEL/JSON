0424252463788d51f165570cdcd02522bd1cfba9
Antonella Enriquez Ledesma <your-email-address~>
2024-06-03 11:48:41 +0200

    add

--- src/js/async/async.js
+++ src/js/async/async.js
@@ -3,9 +3,10 @@ async function logTitle() {
     const curso = await response.json();
     console.log(curso);
   }
+
   fetch('src/js/async/data.json') 
   .then(response => response.json()) 
   .then(data => { data.results.forEach(item => 
     { console.log(item.title); }); }) 
 .catch(error => console.error('Error fetching data:', error));
-  logTitle();
+
