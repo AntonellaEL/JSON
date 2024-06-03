ac455bd9e26eea86ba558afb652150d4ed74e0e2
Antonella Enriquez Ledesma <your-email-address~>
2024-06-03 11:30:00 +0200

    exer part 1

---
+++ src/js/async/async.js
@@ -0,0 +1,11 @@
+async function logTitle() {
+    const response = await fetch("src/js/async/data.json");
+    const curso = await response.json();
+    console.log(curso);
+  }
+  fetch('src/js/async/data.json') 
+  .then(response => response.json()) 
+  .then(data => { data.results.forEach(item => 
+    { console.log(item.title); }); }) 
+.catch(error => console.error('Error fetching data:', error));
+  logTitle();
